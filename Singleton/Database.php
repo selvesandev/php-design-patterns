aecf4a4384acdd3a472d9e247853d53c949cb526
selvesan <dev.selvesan@gmail.com>
2017-10-17 20:16:19 +0545

    singleton pattern

--- Singleton/Database.php
+++ Singleton/Database.php
@@ -1,15 +1,4 @@
-# PHP DESIGN PATTERNS
-Variours design patterns and how to implement them in php.  
-An aggreed upon solution  or a  well defined way to solve a problem.
-
-
-## Singleton 
-Known as a grand parent of all design patterns. A problem that a singleton solves is that it allows 
-one and only one instance of a class in your project.  
-
-Make sure that you have only one instance of the class.
-
-```php
+<?php
 
 class Database
 {
@@ -33,4 +22,4 @@ $db = Database::getInstance();
 $obj = Database::getInstance();
 var_dump($db);//object(Database)#1 (0) { }
 var_dump($obj);//object(Database)#1 (0) { }
-```
+
